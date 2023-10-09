5d20fefae570599db9888f77576bac94eb5a8022
R-Luna75 <68251180+R-Luna75@users.noreply.github.com>
2023-10-09 05:53:36 +0000

    Delete src directory

--- src/main/java/unam/diplomado/pixup/usuariomicroservice/domain/UsuarioAlreadyExistsException.java
+++
@@ -1,9 +0,0 @@
-package unam.diplomado.pixup.usuariomicroservice.domain;
-
-public class UsuarioAlreadyExistsException extends RuntimeException{
-	
-	public UsuarioAlreadyExistsException(String email) {
-		super("Ya existe un usuario registrado con email: "+email);
-	}
-
-}
